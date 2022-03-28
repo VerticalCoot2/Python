2f716087c61879f6ec081b47d0070cd76c932214
verticalcoot2 <verticalcoot2@gmail.com>
2022-03-28 10:28:51 +0200

    weeehee

--- 0328/feladatTelek.py
+++ 0328/feladatTelek.py
@@ -2,7 +2,7 @@ from os.path import exists as ex
 from os import system as sis
 
 def exist():
-    file_exists = ex("H:\Python\0328\tulajok.txt")
+    file_exists = ex("H:\Python\\0328\\negyzet.txt")
     return file_exists
 
 def feladat1(t):
@@ -21,6 +21,34 @@ def feladat3(t):
     hanyadik=meretek.index(legnagyobb)
     print(f"\nLegnagyobb Méretű telek: {legnagyobb} m2")
     print(f"{t[hanyadik][0]} {t[hanyadik][1]} tulajdonna")
+
+def feladat4(t):
+    if(exist()):
+        ki=open("negyzet.txt","a",encoding="utf-8")
+        db=0
+        for i in range(t):
+            if(t[i][2]==t[i][2]):
+                db += 1
+        if db==0:
+            print("\nNincs négyzet alakú telek!")
+            print("Négyzet alakú telek!", file=ki)
+        else:
+            print(f"\n{db}db négyzet alakú telek van")
+            print(f"{db}db négyzet alakú telek van", file=ki)
+        ki.close()
+    else:
+        ki=open("negyzet.txt","w",encoding="utf-8")
+        db=0
+        for i in range(t):
+            if(t[i][2]==t[i][2]):
+                db += 1
+        if db==0:
+            print("\nNincs négyzet alakú telek!")
+            print("Négyzet alakú telek!", file=ki)
+        else:
+            print(f"\n{db}db négyzet alakú telek van")
+            print(f"{db}db négyzet alakú telek van", file=ki)
+        ki.close()
 
 def main():
     sis('cls')
